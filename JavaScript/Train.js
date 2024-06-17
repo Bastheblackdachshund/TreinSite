0e8340476fe18d7a0222aa0c8de1b8678113527c
Bastheblackdachshund <144002097+Bastheblackdachshund@users.noreply.github.com>
2024-06-17 19:34:24 +0200

    finished train presets
    finished train presets and started on the javascript part

--- JavaScript/Train.js
+++ JavaScript/Train.js
@@ -1,3 +1,11 @@
-function selectItem(element) {
-    element.classList.toggle('selected');
+
+onclick = function (e) {
+    let selectedWheel;
+    console.log(e.target.id);
+    if (e.target.id === 'blackWheels') {
+        selectedWheel = 'Black';
+    } else if (e.target.id === 'redWheels') {
+        selectedWheel = 'Red';
+    }
+    console.log("Selected wheel type:", selectedWheel);
 }
